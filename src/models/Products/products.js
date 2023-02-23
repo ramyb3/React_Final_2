760ef3cf089272de0aa340781643fa605baa911c
ramy <ramy_3@walla.com>
2023-02-23 19:29:07 +0200

    litle changes

--- src/models/Products/products.js
+++ src/models/Products/products.js
@@ -1,7 +1,7 @@
+import Comp1 from "./comp1";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import Comp1 from "./comp1";
 
 export default function Products() {
   const storeData = useSelector((state) => state);
