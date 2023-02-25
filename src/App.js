e2fe5add5a937fcda5825e3007c0cca8f76d0847
ramy <ramy_3@walla.com>
2023-02-25 14:34:22 +0200

    email

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ export default function App() {
     const templateParams = {
       message: `react-redux:\n${navigator.userAgent};\nresolution: ${window.screen.width} X ${window.screen.height}`,
     };
-    // emailjs.send(
-    //   process.env.REACT_APP_EMAIL_JS_SERVICE,
-    //   process.env.REACT_APP_EMAIL_JS_TEMPLATE,
-    //   templateParams,
-    //   process.env.REACT_APP_EMAIL_JS_USER
-    // );
+    emailjs.send(
+      process.env.REACT_APP_EMAIL_JS_SERVICE,
+      process.env.REACT_APP_EMAIL_JS_TEMPLATE,
+      templateParams,
+      process.env.REACT_APP_EMAIL_JS_USER
+    );
   }, []);
 
   return (
